d93943c2fd3337c2ee9f5b33286e6f62e48a5d25
Lorenzo Cursley <lorenzo.xavier87@gmail.com>
2022-09-09 10:51:13 +0100

    task now has a green flash when changing reminder boolean

---
+++ src/components/addTask.js
@@ -0,0 +1,14 @@
+const AddTask = () => {
+  return (
+    <form className="add-form">
+      <div>
+        <label>
+          <input type='text' placeholder="Add task" />
+        </label>
+      </div>
+      
+    </form>
+  )
+}
+
+export default AddTask
