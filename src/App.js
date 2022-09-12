bbe7ba1bb89fe637d19af67393d2953bf9765192
Lorenzo Cursley <lorenzo.xavier87@gmail.com>
2022-09-12 20:31:25 +0100

    changed the default form to be closed

--- src/App.js
+++ src/App.js
@@ -96,7 +96,7 @@ const App = () => {
             path='/'
             element={
             <>
-              { showAddTask && <AddTask onAdd={addTask} /> }
+              { !showAddTask && <AddTask onAdd={addTask} /> }
               {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> : 'No Tasks to show'}
             </>
             }
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header({title, onAdd, showAdd}) {
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button  color={showAdd ? "red" : "green" } text={showAdd ? 'Close' : 'Add'} onClick={onAdd} />
+      <Button  color={showAdd ? "green" : "red" } text={showAdd ? 'Add' : 'Close'} onClick={onAdd} />
     </header>
   )
 }
