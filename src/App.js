fac79bc94d82172a8b783bc92f41717862b79f1b
Lorenzo Cursley <lorenzo.xavier87@gmail.com>
2022-09-12 16:30:43 +0100

    new task now persists in DB

--- src/App.js
+++ src/App.js
@@ -27,10 +27,22 @@ const App = () => {
   }
 
   // Add task
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 1000) + 1
-  const newTask = { id, ...task }
-    setTasks([...tasks, newTask])
+  const addTask = async (task) => {
+    const res = await fetch(`http://localhost:5000/tasks/`, {
+        method: 'POST',
+        headers: { 
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(task),
+      })
+
+    const data = await res.json()
+
+    setTasks([...tasks, data])
+    
+    //   const id = Math.floor(Math.random() * 1000) + 1
+    // const newTask = { id, ...task }
+    //   setTasks([...tasks, newTask])
   }
 
   // delete task
